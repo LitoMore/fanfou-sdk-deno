744df3cce25357bb727d9fd9d29dcb6042fac123
LitoMore <litomore@gmail.com>
2020-05-17 16:19:33 +0800

    Add protocol

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ export default class Fanfou {
       password = "",
       apiDomain = "api.fanfou.com",
       oauthDomain = "fanfou.com",
-      protocol = "",
+      protocol = "http:",
       hooks = {}
     } = opt;
 
